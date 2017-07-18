2d1242190f4ed7d815a06e2c69aaf2ebc98b53f9
Marco Emrich <m.emrich@webmasters.de>
2017-07-19 00:40:22 +0200

    improve Immutable examples

--- immutable_examples.spec.js
+++ immutable_examples.spec.js
@@ -1,7 +1,7 @@
 const expect = require('unexpected');
 const { Map } = require('immutable');
 
-describe("immutable", () => {
+describe("Immutable", () => {
   it("should create a new obj with different values", () => {
     const map1 = Map({ a: 1, b: 2, c: 3 });
     const map2 = map1.set('b', 50);
--- immutable_ramda_examples.spec.js
+++ immutable_ramda_examples.spec.js
@@ -2,7 +2,7 @@ const expect = require('unexpected');
 const I = require('./ramda-immutable-wrapper');
 const { Map } = require('immutable');
 
-describe("immutable", () => {
+describe("Immutable (integrated with Ramda)", () => {
   it("should create a new obj with different values", () => {
     const map1 = Map({ a: 1, b: 2, c: 3 });
     const map2 = I.set('b', 50, map1);
